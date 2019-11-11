4cc037cb48ccca1478d9f1f10752e1ee3e203d49
João Felipe <jo_5919@hotmail.com>
2019-11-10 22:01:37 -0300

    Adds email configuration and email template

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '7bb558982dd1ac',
+    pass: 'eda0aea76004a7',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
